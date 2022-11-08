857af9afee6884135884cb0d632771972467bfcd
Guilherme Santana <g.santossantana22@gmail.com>
2022-11-08 09:47:09 -0300

    tela privacidade

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar, StyleSheet } from "react-native";
 import Favoritos from "./src/screens/Favoritos";
 import FormBusca from "./src/screens/FormBusca";
 import Home from "./src/screens/Home";
+import Privacidade from "./src/screens/Privacidade";
+import Sobre from "./src/screens/Sobre";
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="default" />
-      <Favoritos />
+      <Privacidade />
     </>
   );
 };
