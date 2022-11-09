347d0d882afb8dbb10fc12502cd9768052ab4d60
Guilherme Santana <g.santossantana22@gmail.com>
2022-11-09 10:01:50 -0300

    finalização exercicio

--- src/screens/FormBusca.js
+++ src/screens/FormBusca.js
@@ -1,14 +1,66 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+} from "react-native";
+
+import { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const FormBusca = () => {
+  /* state para o filme que será buscado */
+  const [filme, setFilme] = useState("");
+
+  /* evento de captura do texinput a partir do onchangetext */
+  const capturaDigitacao = (valor) => {
+    setFilme(valor);
+  };
+
+  const inputTexto = () => {
+    if (!filme) {
+      Alert.alert("Ops!", "Você deve digitar o nome de um filme"),
+        [{ filme: "OK" }];
+      return;
+    }
+    Alert.alert("Você procurou por:", filme), [{ filme: "OK" }];
+  };
+
   return (
-    <SafeAreaView>
-      <Text>FormBusca</Text>
+    <SafeAreaView style={estilos.container}>
+      <Text>Star Trek? O Poderoso Chefão? A trilogia Senhos dos Anéis?</Text>
+      <Text>Localize um filme que você viu ou gostaria de ver!</Text>
+      <View style={estilos.caixaInput}>
+        <Ionicons name="film" size={44} color="black" />
+        <TextInput
+          style={estilos.input}
+          onChangeText={capturaDigitacao}
+          placeholder="Filme..."
+        />
+      </View>
+      <Button title="Buscar" color="#5451a6" onPress={inputTexto} />
     </SafeAreaView>
   );
 };
 
 export default FormBusca;
 
-const styles = StyleSheet.create({});
+const estilos = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    flex: 1,
+  },
+  caixaInput: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
